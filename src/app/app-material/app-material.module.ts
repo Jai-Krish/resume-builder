4b04f2e22bbca2a5907f73e4a7bc327ea852b5a6
Jai Krishnan <mailtojaikrish@gmail.com>
2019-06-10 18:51:57 +0530

    Style the zoom and save button

--- src/app/app-material/app-material.module.ts
+++ src/app/app-material/app-material.module.ts
@@ -3,6 +3,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   exports: [
@@ -10,6 +11,7 @@ import { MatCardModule } from '@angular/material/card';
     MatInputModule,
     MatSelectModule,
     MatCardModule,
+    MatButtonModule,
   ],
 })
 export class AppMaterialModule {}
